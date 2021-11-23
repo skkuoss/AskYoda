77c94ce398f8002227d093fe9297d74c5c113fd2
BestevenSKKU <nikkuneimk@g.skku.edu>
2021-11-23 17:43:54 +0900

    [Edit] recommend router

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// handle /v1/recommendations
 router.get('/', function (req, res, next) {
+  // req.query => get all querystring
+  // req.params => get all paths (In this router, there is no params)
+
+  // response value <= all querystrings wrapping by javascript object
   res.send(req.query);
 });
 
