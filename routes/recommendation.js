cb2a6568bc1607c7ab26e86eca1166eda11b3f96
BestevenSKKU <nikkuneimk@g.skku.edu>
2021-11-23 19:29:39 +0900

    [Edit] recommendation route

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -7,7 +7,25 @@ router.get('/', function (req, res, next) {
   // req.params => get all paths (In this router, there is no params)
 
   // response value <= all querystrings wrapping by javascript object
-  res.send(req.query);
+  // res.send(req.query);
+
+  // Send Our songs to spotify server
+  // song1_url
+  // song2_url
+  console.log(req.query.songs)
+  console.log(req.query.songs[0])
+  console.log(req.query.songs[1])
+
+  // Algoritehms Part start
+  // Algoritehms Part end
+
+  res.send({
+    name: 'Beatles - Yellow submarine'
+  });
+});
+
+router.post('/', function (req, res) {
+  res.send('Post Response');
 });
 
 module.exports = router;
