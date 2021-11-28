278604c98460034256899a8dd7db6e532dca9502
BestevenSKKU <nikkuneimk@g.skku.edu>
2021-11-28 20:17:19 +0900

    [Add] README

--- __tests__/recommendation.js
+++ __tests__/recommendation.js
@@ -1,14 +1,34 @@
 const axios = require('axios');
 
 describe('Test recommendation API', () => {
-    test('/recommendations GET', async () => {
-        const res = await axios.get('http://localhost:3100/v1/recommendations');
+    test('/autocomplete GET - Gangname Style', async () => {
+        const res = await axios.get('http://localhost:3100/v1/autocomplete?trackName=gan');
         expect({
             status: res.status,
-            data: res.data
+            name: res.data.body.tracks.items[2].name
         }).toEqual({
             status: 200,
-            data: 'test'
+            name: 'Gangnam Style (강남스타일)'
+        });
+    });
+    test('/autocomplete GET - Here Comes The Sun', async () => {
+        const res = await axios.get('http://localhost:3100/v1/autocomplete?trackName=here');
+        expect({
+            status: res.status,
+            name: res.data.body.tracks.items[0].name
+        }).toEqual({
+            status: 200,
+            name: 'Here Comes The Sun - Remastered 2009'
+        });
+    });
+    test('/recommendations GET', async () => {
+        const res = await axios.get('http://localhost:3100/v1/recommendations?songs=gan&songs=Here%20Comes%20The%20Sun%20-%20Remastered%202009%20-%20The%20Beatles');
+        expect({
+            status: res.status,
+            length: res.data.tracks.length
+        }).toEqual({
+            status: 200,
+            length: 20
         });
     });
     test('/recommendations POST', async () => {
